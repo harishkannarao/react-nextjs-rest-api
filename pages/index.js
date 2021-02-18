1a41b0b163ab56ce021467b1ac05aab3d93ac590
harishkannarao <harish.gtec@gmail.com>
2021-02-18 22:43:17 +0000

    Adding favicon

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,14 @@ import React from 'react';
 export class HomePage extends React.Component {
     render() {
         return (
-            <div data-testid="message">Welcome to Next.js!!!</div>
+            <html>
+                <head>
+                    <title>Next.js</title>
+                </head>
+                <body>
+                    <div data-testid="message">Welcome to Next.js!!!</div>
+                </body>
+            </html>           
         );
     }
 }
