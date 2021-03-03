8df246ac81e4ca12d9f251031134bd2f03a48bb9
harishkannarao <harish.gtec@gmail.com>
2021-03-03 17:18:59 +0000

    Writing test for functional component

---
+++ __tests__/components/react-fetch/stats.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { render, fireEvent, screen } from '@testing-library/react'
+import { server } from '../../server'
+import { rest } from 'msw'
+
+import { Stats } from "../../../components/react-fetch/stats";
+
+describe('Example Stats Component test', () => {
+    test('displays stats data', async () => {
+        render(
+            <Stats
+                subscribers_count='200'
+                stargazers_count='300'
+                forks_count='150'
+            />
+        )
+        expect(screen.queryByTestId('subscribers_count').textContent).toBe('200');
+        expect(screen.queryByTestId('stargazers_count').textContent).toBe('300');
+        expect(screen.queryByTestId('forks_count').textContent).toBe('150');
+    });
+});
