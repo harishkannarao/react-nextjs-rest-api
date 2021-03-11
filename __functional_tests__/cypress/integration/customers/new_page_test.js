9b5df4b3fa3a32f3654f43e14e0156693469a22d
harishkannarao <harish.gtec@gmail.com>
2021-03-11 14:22:25 +0000

    Adding test to create new customer

--- __functional_tests__/cypress/integration/customers/new_page_test.js
+++ __functional_tests__/cypress/integration/customers/new_page_test.js
@@ -19,7 +19,40 @@ describe('Test New Customer Page', () => {
     });
 
     it('creates new customer', () => {
+        cy.intercept('POST', Cypress.env('CUSTOMER_API_BASE_URL') + '/customers', (req) => {
+            req.alias = 'createCustomer';
+            req.reply(
+                {
+                    statusCode: 200,
+                    delay: 500,
+                    body: {}
+                }
+            );
+        });
 
+        cy.intercept(Cypress.env('CUSTOMER_API_BASE_URL') + '/customers',
+            {
+                statusCode: 200,
+                delay: 0,
+                body: []
+            }
+        )
+
+        cy.visit("/customers/new/");
+
+        cy.get('[data-testid="first-name"]').clear().type('test-first-name');
+        cy.get('[data-testid="last-name"]').clear().type('test-last-name');
+        cy.get('[data-testid="submit-button"]').click();
+
+        cy.get('[data-testid="submitting-content"]').should('exist');
+
+        cy.wait('@createCustomer').then((interception) => {
+            var reqJson = interception.request.body;
+            expect(reqJson.firstName).to.equal('test-first-name');
+            expect(reqJson.lastName).to.equal('test-last-name');
+        });
+
+        cy.title().should('eq', 'List - Customers');
     });
 
     it('displays error from api', () => {
@@ -36,7 +69,7 @@ describe('Test New Customer Page', () => {
 
         cy.get('[data-testid="submitting-content"]').should('exist');
         cy.get('[data-testid="submitting-content"]').should('not.exist');
-        
+
         cy.get('[data-testid="error-content"]').should('contain', 'unit-test-error');
         cy.get('[data-testid="error-content"]').should('contain', 'Internal Server Error');
     });
