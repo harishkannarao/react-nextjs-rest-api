54c5b4f568fea8162218a7c92753808b58c98488
harishkannarao <harish.gtec@gmail.com>
2021-03-12 21:34:30 +0000

    Deleting sample files

--- components/react-fetch/stats.js
+++
@@ -1,9 +0,0 @@
-export function Stats(props) {
-    return (
-        <div>
-            <strong>👀 <span data-testid="subscribers_count">{props.subscribers_count}</span></strong>{" "}
-            <strong>✨ <span data-testid="stargazers_count">{props.stargazers_count}</span></strong>{" "}
-            <strong>🍴 <span data-testid="forks_count">{props.forks_count}</span></strong>
-        </div>
-    )
-}
