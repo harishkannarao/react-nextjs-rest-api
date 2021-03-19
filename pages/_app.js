6913002deea9e6ee518eb6571e372f34c3c999b3
harishkannarao <harish.gtec@gmail.com>
2021-03-19 12:51:56 +0000

    Adding bootstrap

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../css/global.css'
+import Head from 'next/head'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
+}
