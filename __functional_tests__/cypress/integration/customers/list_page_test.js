b4c40e963ded26f04e3d617f9778e1f29f9dcb2b
harishkannarao <harish.gtec@gmail.com>
2021-03-23 14:38:10 +0000

    Testing delete customer journey

--- __functional_tests__/cypress/integration/customers/list_page_test.js
+++ __functional_tests__/cypress/integration/customers/list_page_test.js
@@ -43,4 +43,58 @@ describe('Test Customer List Page', () => {
 
         cy.title().should('eq', 'List - Customers');
     });
+
+    it('Deletes customer', () => {
+        var listCustomersCount = 0;
+        cy.intercept('GET', Cypress.env('CUSTOMER_API_BASE_URL') + '/customers', (req) => {
+            req.alias = 'listCustomers-' + listCustomersCount;
+            listCustomersCount = listCustomersCount + 1;
+            req.reply(
+                {
+                    statusCode: 200,
+                    delay: 500,
+                    body: [
+                        {
+                            id: 1,
+                            firstName: 'test-first-name-1',
+                            lastName: 'test-last-name-1'
+                        },
+                        {
+                            id: 2,
+                            firstName: 'test-first-name-2',
+                            lastName: 'test-last-name-2'
+                        }
+                    ]
+                }
+            );
+        });
+
+        cy.intercept('DELETE', Cypress.env('CUSTOMER_API_BASE_URL') + '/customers', (req) => {
+            req.alias = 'deleteCustomer';
+            req.reply(
+                {
+                    statusCode: 200,
+                    delay: 500,
+                    body: null
+                }
+            );
+        });
+
+        cy.visit("/customers/list/");
+
+        cy.wait('@listCustomers-0');
+
+        cy.get('[data-testid="delete-button"]').eq(1).click();
+
+        cy.get('[data-testid="processing-content"]').should('exist');
+
+        cy.get('[data-testid="processing-content"]').should('not.exist');
+
+        cy.wait('@listCustomers-1');
+
+        cy.wait('@deleteCustomer').then((interception) => {
+            var reqJson = interception.request.body;
+            expect(reqJson.id).to.equal(2);
+        });
+    });
 });
