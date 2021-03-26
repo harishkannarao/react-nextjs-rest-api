5bf54503961883d4ee9bcd4ed49cfffc1f5f6e5d
harishkannarao <harish.gtec@gmail.com>
2021-03-26 18:54:10 +0000

    Supressing console.error messages

--- __tests__/setup-env.js
+++ __tests__/setup-env.js
@@ -1,6 +1,13 @@
 import {server} from './server.js'
 
 beforeAll(() => server.listen())
-beforeEach(() => server.resetHandlers())
-afterEach(() => server.resetHandlers())
+beforeEach(() => {
+    server.resetHandlers();
+    jest.spyOn(console, 'error');
+    console.error.mockImplementation(() => null);
+})
+afterEach(() => {
+    server.resetHandlers();
+    console.error.mockRestore();
+})
 afterAll(() => server.close())
