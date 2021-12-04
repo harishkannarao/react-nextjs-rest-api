221aba5852899133234207a420bf27b9b5b67112
harishkannarao <harishkannarao@users.noreply.github.com>
2021-12-04 13:09:55 +0000

    Adding unit tests for api

--- components/common/api.js
+++ components/common/api.js
@@ -1,13 +1,17 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
+
+const instance = axios.create({
+  headers: {'X-Custom-Header': 'foo-bar'}
+});
 
 export function executeGet(url, queryParams, timeoutMillis = 1000) {
-    return axios.get(url, {params: queryParams, timeout: timeoutMillis});
+    return instance.get(url, {params: queryParams, timeout: timeoutMillis});
 };
 
 export function executePost(url, data, timeoutMillis = 1000) {
-  return axios.post(url, data, {timeout: timeoutMillis});
+  return instance.post(url, data, {timeout: timeoutMillis});
 };
 
 export function executeDelete(url, data, timeoutMillis = 1000) {
-  return axios.delete(url, {data: data, timeout: timeoutMillis});
+  return instance.delete(url, {data: data, timeout: timeoutMillis});
 };
